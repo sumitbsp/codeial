69b051805009b1bb75582ae9f18572fe8deec69f
sumitbsp <sumit.bsp88@gmail.com>
2020-01-30 02:02:52 +0530

    replace some controller's function with async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,25 +1,27 @@
 const Post = require('../models/post');
 const User = require('../models/users');
 
-module.exports.home = function (req, res) {
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts) {
+module.exports.home = async function (req, res) {
 
-        User.find({}, function(err, users) {
-            return res.render('home', {
-                title: 'Codeial | Home',
-                posts: posts,
-                all_users: users
-            })
-        })
-
-       
-    })
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+    
+        let users = await User.find({});
+    
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts,
+            all_users: users
+        });
+    }catch (err) {
+        console.log('error', err);
+        return;
+    }
 };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,26 +1,25 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res) {
-    Post.create({
+module.exports.create = async function(req, res) {
+    await Post.create({
         content: req.body.content,
         user: req.user._id
-    }, function(err, post) {
-        if(err){console.log('error in creating post'); return}
-        return res.redirect('back');
     });
+    return res.redirect('back');
 }
 
-module.exports.destroy = function (req, res) {
-    Post.findById(req.params.id, function(err, post) {
+module.exports.destroy = async function (req, res) {
+    try{
+        let post = await Post.findById(req.params.id);
+    
         if(post.user == req.user.id) {
             post.remove();
-
-            Comment.deleteMany({post: req.params.id}, function(err) {
-                return res.redirect('back');
-            });
-        } else {
+            await Comment.deleteMany({post: req.params.id});
             return res.redirect('back');
         }
-    })
+    } catch(err) {
+        console.log('error', err); return;
+    }
+    
 }
