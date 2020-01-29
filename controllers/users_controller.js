1734dad03ea1f8d9022d2fbb1d37669ba84664aa
sumitbsp <sumit.bsp88@gmail.com>
2020-01-30 00:02:21 +0530

    add delete posts and comments and update profile info of the logged in user. Also styles the home page a bit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,22 @@
 const User = require('../models/users');
 
 module.exports.profile = function(req, res) {
-    return res.render('profile')
+    User.findById(req.params.id, function(err, user) {
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        })
+    })
+}
+
+module.exports.update = function(req, res) {
+    if (req.user.id == req.params.id) {
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
+            return res.redirect('back');
+        })
+    } else {
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 // render sign up page
